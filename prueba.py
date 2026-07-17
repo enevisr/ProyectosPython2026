81b6fbe4fe1fe957923f24d1cba75407c56df941
Rafael Reyes <enevisr@gmail.com>
2026-07-17 09:55:17 -0500

    Trabajo de actualización caso

---
+++ prueba.py
@@ -0,0 +1,12 @@
+import tkinter 
+# from tkinter import *
+
+# creación del contenedor.
+ventana = tkinter.Tk()
+ventana.geometry("300x300")
+ventana.mainloop()
+
+
+
+#cuadro = tk.Style()
+        #self.estilo.theme_use("clam")
